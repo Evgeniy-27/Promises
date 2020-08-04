dcfc5c52410c89ca3f471a26678781cc7c644a3b
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-08-04 22:39:46 +0700

    Готово вер.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import GameSavingLoader from './gameSavingLoader';
 
-GameSavingLoader.load().then((saving) => {
+GameSavingLoader.load()
+.then((saving) => {
   console.log(saving);
 }, (error) => {
   console.error(error);
